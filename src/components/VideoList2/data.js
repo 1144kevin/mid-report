05af85230bafa783b1c06758ca61544c502b3e15
jaina0831 <103718281+jaina0831@users.noreply.github.com>
2023-04-10 00:26:41 +0800

    Add files via upload

---
+++ src/components/VideoList2/data.js
@@ -0,0 +1,21 @@
+export const data = [
+  { 
+    "id": "00",
+    "image1": "/images/video1-1.jpg",
+    "info1":"紙房子",
+    "image2": "/images/video1-2.png",
+    "info2":"性愛自修室",
+    "image3":"/images/video1-3.jpg",
+    "info3":"雨傘學院",
+    "image4": "/images/video1-4.png",
+    "info4":"愛蜜莉在巴黎",
+    "image5": "/images/video1-5.jpeg",
+    "info5":"宅男行不行",
+    "image6": "/images/video1-6.jpg",
+    "info6":"爆笑超市",
+    "image7": "/images/video1-7.jpeg",
+    "info7":"星期三",
+    "image8": "/images/video1-8.jpeg",
+    "info8":"荒唐分局"
+  },
+]
